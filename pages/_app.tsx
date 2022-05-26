3f2eec866aeb8cd8a909ba1d905838a104867e65
Solit0123 <lodisolito@gmail.com>
2022-05-26 19:20:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from "next/app"
+import { ApolloProvider } from "@apollo/client"
+import { useApollo } from "../utils/apollo"
+import Head from "next/head"
+
+// Add bootstrap css
+import "bootstrap/dist/css/bootstrap.css"
+// Custom css
+import "../styles/globals.css"
+
+function App({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApolloState)
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
