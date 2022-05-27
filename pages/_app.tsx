31734898791dfffe80f4303b838b508a811da313
Solit0123 <61633743+Solit0123@users.noreply.github.com>
2022-05-27 00:01:09 -0700

    fixing the component thing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,6 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
-      
     </>
   )
 }
